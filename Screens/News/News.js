775dc54355f9e4f0c6c78e00697f9f82aedd23c0
Casey6624 <34159052+Casey6624@users.noreply.github.com>
2019-05-07 19:20:01 +0100

    Stripped Excerpts of HTML tags, now using regex to transform the excerpt into a more readable format

--- Screens/News/News.js
+++ Screens/News/News.js
@@ -19,15 +19,26 @@ export default class NewsScreen extends Component {
         axios.get(queryURL)
             .then(res => {
                 this.setState({ blogData: res.data })
+                console.log(res.data[0])
             })
             .catch(err => {
                 console.log(err)
             })
     }
 
+    stripHTML = (rawHTML) => {
+        console.log(rawHTML)
+        const regex = /(<([^>]+)>)/ig;
+        let strippedExcerpt = rawHTML.replace(regex, '');
+        if (strippedExcerpt.includes("[&hellip;]")) {
+            return strippedExcerpt.split("[&hellip;]")[0] + "..."
+        }
+        return strippedExcerpt
+    }
+
     render() {
         if (this.state.pullingBlogsInProgress) {
-            <View>
+            <View style={styles.container}>
                 <Text style={styles.loadingBlogs}>Loading...</Text>
             </View>
         }
@@ -35,18 +46,23 @@ export default class NewsScreen extends Component {
         return (
             <ScrollView style={styles.container}>
                 <Text style={styles.newsTitle}> NEWS | <Text style={styles.headerAccent}>JOLLY  IT</Text></Text>
-                {this.state.blogData.map((blog, index) =>
-                    <View key={blog.id}>
-                        <Text style={styles.blogTitle}>{blog.title.rendered}</Text>
-                        <Image
-                            style={{ width: 150, height: 150 }}
-                            source={{ uri: blog._embedded['wp:featuredmedia'][0].media_details.sizes.thumbnail.source_url }}
-                        />
-                        <TouchableOpacity style={styles.viewMoreBtn} onPress={() => Linking.openURL(blog.link)}>
-                            <Text style={styles.viewMoreBtnTxt}> View Full </Text>
-                        </TouchableOpacity>
-                    </View>
-                )}
+                <View style={styles.innerContainer}>
+                    {this.state.blogData.map((blog, index) =>
+                        <View key={blog.id} style={styles.blog}>
+                            <Text style={styles.blogTitle}>{blog.title.rendered}</Text>
+                            <Image
+                                style={{ width: 150, height: 150, borderRadius: 5 }}
+                                source={{ uri: blog._embedded['wp:featuredmedia'][0].media_details.sizes.thumbnail.source_url }}
+                            />
+                            <Text>
+                                {this.stripHTML(blog.excerpt.rendered)}
+                            </Text>
+                            <TouchableOpacity style={styles.viewMoreBtn} onPress={() => Linking.openURL(blog.link)}>
+                                <Text style={styles.viewMoreBtnTxt}> View Full Post</Text>
+                            </TouchableOpacity>
+                        </View>
+                    )}
+                </View>
             </ScrollView>
         );
     }
@@ -57,31 +73,43 @@ const styles = StyleSheet.create({
         backgroundColor: "#2A2F33",
         flex: 1,
     },
+    innerContainer: {
+        padding: 10
+    },
+    blog: {
+        padding: 5
+    },
     viewMoreBtn: {
         backgroundColor: "#ef7d00",
         borderRadius: 5,
         padding: 5,
-        width: 150,
+        width: "auto",
         height: 50,
-        textAlign: "center"
+        margin: 15,
+        textAlign: "center",
+        justifyContent: "center",
+        flex: 1
     },
     viewMoreBtnTxt: {
         textAlign: "center",
         color: "white",
         fontSize: 18,
+        alignSelf: "center",
+        justifyContent: "center",
         fontFamily: Fonts.RobotoBold,
     },
     newsTitle: {
         color: "white",
         fontSize: 35,
         marginTop: 10,
-        textAlign: "center",
-        fontFamily: Fonts.RobotoLight,
-        paddingBottom: 5
+        textAlign: "center"
     },
     blogTitle: {
         color: "white",
-        fontSize: 22
+        fontSize: 26,
+        textAlign: "center",
+        fontFamily: Fonts.RobotoBold,
+        paddingBottom: 15,
     },
     headerAccent: {
         color: "#ef7d00"
