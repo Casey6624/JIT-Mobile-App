0687bf3f22fb9d9617d942a77e7c99128cf31784
Casey6624 <34159052+Casey6624@users.noreply.github.com>
2019-05-13 16:46:27 +0100

    More Modal work

--- Screens/About/Modals/ITHelpdesk.js
+++ Screens/About/Modals/ITHelpdesk.js
@@ -1,12 +1,39 @@
 import React from "react"
-import { View, Text } from "react-native"
+import Fonts from "../../../utils/fonts"
+import SquareLogo from "../../../assets/img/jollyIcon.png"
+import { View, Text, Image } from "react-native"
 
 const ITHelpdesk = props => {
     return (
-        <View>
-            <Text> Hello! From the ITHelpdesk Component </Text>
+        <View style={styles.container}>
+            <Text style={styles.ModalTitle}> IT HELPDESK <Image source={SquareLogo} /> </Text>
+            <View style={styles.mainContent}>
+                <Text style={styles.tagLine}>
+                    Something for small businesses, large businesses and everyone inbetween.
+            </Text>
+            </View>
         </View>
     )
+}
+
+const styles = {
+    container: {
+        flex: 1
+    },
+    mainContent: {
+        margin: 10
+    },
+    ModalTitle: {
+        fontFamily: Fonts.RobotoBold,
+        fontSize: 32,
+        color: "white",
+        textAlign: "center"
+    },
+    tagLine: {
+        color: "white",
+        fontSize: 28,
+        fontFamily: Fonts.RobotoLight,
+    }
 }
 
 export default ITHelpdesk
