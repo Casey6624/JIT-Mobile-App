9dd9236d5acbc175bbba8ed4b135c98c150cd617
Casey Smith <34159052+Casey6624@users.noreply.github.com>
2019-06-03 13:48:39 +0100

    64bit testing MERGE (#2)
    * Rewrote the home screen using hooks. Implemented a useInterval hook which refreshes the LMI api call every 10 seconds
    
    * New Modal implemented for IT director and undertaking styling changes for bigger screens

--- Screens/Home/Home.js
+++ Screens/Home/Home.js
@@ -1,5 +1,5 @@
 // Libaries
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import { StyleSheet, Text, View, Image, Animated, ImageBackground, TouchableOpacity, Easing, Linking, Dimensions } from 'react-native';
 import { TabNavigator, TabBarBottom, createBottomTabNavigator } from "react-navigation";
 import axios from "axios"
@@ -16,165 +16,184 @@ import JollyRobot from "../../assets/img/JollyRobotCirc.png";
 import NewsScreen from "../News/News";
 import SettingsScreen from "../Settings/Settings"
 import AboutScreen from "../About/About";
+// libraries
+import moment from "moment"
+// Custom hooks
+import { useInterval } from "../../hooks/index"
 
-class HomeScreen extends Component {
+function HomeScreen(props) {
 
-    state = {
-        numOfCustomers: null,
-        noData: false,
-        showAnim: new Animated.Value(0)
-    }
 
-    componentDidMount() {
+    const [numOfCustomers, setNumOfCustomers] = useState(null)
+    const [noData, setNoData] = useState(false)
+    const [showAnim, setShowAnim] = useState(new Animated.Value(0))
+    const [lastUpdated, setLastUpdated] = useState(null)
+
+    useEffect(() => {
+        fetchSessions()
+        console.log(Dimensions.get("window").height)
+        console.log(Dimensions.get("window").width)
+    }, [])
+
+    function fetchSessions() {
         const url = "http://tasks.jollyit.co.uk/php/LMI/totalSessions.php"
         axios.get(url)
             .then(res => {
                 if (res.status === 200 && !isNaN(res.data)) {
                     if (res.data > 0) {
-                        this.setState({ numOfCustomers: res.data })
+                        setNumOfCustomers(res.data)
                     }
+                    const updated = new Date().toISOString()
+                    setLastUpdated(moment(updated).format('LTS'))
                     return
                 }
             })
             .catch(err => {
+                if (numOfCustomers !== null) return
                 console.log(err)
-                this.setState({ noData: true })
+                noData(true)
             })
     }
 
-    animateBoxes = () => {
-        Animated.timing(this.state.showAnim, {
+    useInterval(() => {
+        fetchSessions()
+        const updated = new Date().toISOString()
+        setLastUpdated(moment(updated).format('LTS'))
+    }, 10000)
+
+    function animateBoxes() {
+        Animated.timing(showAnim, {
             toValue: 1,
             duration: 600,
             useNativeDriver: true
         }).start()
     }
 
-    render() {
+    return (
+        <ImageBackground style={styles.container} source={SheffieldTrain} onLoad={() => animateBoxes()}>
+            <View style={styles.headerText}>
 
-        return (
-            <ImageBackground style={styles.container} source={SheffieldTrain} onLoad={this.animateBoxes()}>
-                <View style={styles.headerText}>
+                <Text style={styles.welcomeText}>JOLLY IT</Text>
 
-                    <Text style={styles.welcomeText}>JOLLY IT</Text>
-
-                    <Icon name="md-home" size={35} style={{ color: "#2A2F33" }} />
-                </View>
-                <View style={styles.contentContainer}
-                >
-                    <Animated.View style={{
-                        opacity: this.state.showAnim, transform: [
-                            {
-                                translateX: this.state.showAnim.interpolate({
-                                    inputRange: [0, 1],
-                                    outputRange: [1, 20]
-                                })
-                            }
-                        ]
-                    }}>
-                        <Text
-                            style={{
-                                fontSize: 38,
-                                color: "white",
-                                backgroundColor: "#2A2F33",
-                                padding: 5,
-                                fontFamily: Fonts.RobotoLight,
-                                borderRadius: 10
-                            }}>
-                            ...a family run IT consultancy based in London 💂 + Yorkshire ☕.
+                <Icon name="md-home" size={35} style={{ color: "#2A2F33" }} />
+            </View>
+            <View style={styles.contentContainer}
+            >
+                <Animated.View style={{
+                    opacity: showAnim, transform: [
+                        {
+                            translateX: showAnim.interpolate({
+                                inputRange: [0, 1],
+                                outputRange: [1, 20]
+                            })
+                        }
+                    ]
+                }}>
+                    <Text
+                        style={{
+                            fontSize: 38,
+                            color: "white",
+                            backgroundColor: "#2A2F33",
+                            padding: 5,
+                            fontFamily: Fonts.RobotoLight,
+                            borderRadius: 10
+                        }}>
+                        ...a family run IT consultancy based in London 💂 + Yorkshire ☕.
                         </Text>
-                    </Animated.View>
-                    <Animated.View style={{
-                        flexDirection: "row",
-                        justifyContent: "space-around",
-                        margin: 12,
-                        transform: [
-                            {
-                                scale: this.state.showAnim.interpolate({
-                                    inputRange: [0, 1],
-                                    outputRange: [20, 1]
-                                })
-                            }
-                        ]
-                    }}>
-                        <Icon name="logo-twitter" size={36} style={{ color: "#ef7d00" }}
-                            onPress={() => Linking.openURL("https://twitter.com/jollyit")}
-                        />
-                        <Icon name="logo-facebook" size={36} style={{ color: "#ef7d00" }}
-                            onPress={() => Linking.openURL("https://www.facebook.com/JollyIT")}
-                        />
-                        <Icon name="logo-linkedin" size={36} style={{ color: "#ef7d00" }}
-                            onPress={() => Linking.openURL("https://www.linkedin.com/company/jolly-it-solutions-ltd")}
-                        />
-                        <Icon name="logo-youtube" size={36} style={{ color: "#ef7d00" }}
-                            onPress={() => Linking.openURL("https://www.youtube.com/channel/UCAX1Fq1g7mQv82ZQ0vO-84A")} />
-                    </Animated.View>
-                    <Animated.View style={{
-                        marginTop: 10,
-                        fontFamily: Fonts.RobotoLight,
-                        borderRadius: 5,
-                        textAlign: "center",
-                        backgroundColor: "#2A2F33",
-                        opacity: this.state.showAnim,
-                        width: 320,
-                        padding: 5,
-                        justifyContent: "space-between",
-                        transform: [
-                            {
-                                translateX: this.state.showAnim.interpolate({
-                                    inputRange: [0, 1],
-                                    outputRange: [20, 1]
-                                })
-                            },
-                            {
-                                translateY: this.state.showAnim.interpolate({
-                                    inputRange: [0, 1],
-                                    outputRange: [20, 1]
-                                })
-                            }
-                        ]
-                    }}>
-                        {this.state.numOfCustomers && <Text style={styles.LMIText}>
+                </Animated.View>
+                <Animated.View style={{
+                    flexDirection: "row",
+                    justifyContent: "space-around",
+                    margin: 12,
+                    transform: [
+                        {
+                            scale: showAnim.interpolate({
+                                inputRange: [0, 1],
+                                outputRange: [20, 1]
+                            })
+                        }
+                    ]
+                }}>
+                    <Icon name="logo-twitter" size={36} style={{ color: "#ef7d00" }}
+                        onPress={() => Linking.openURL("https://twitter.com/jollyit")}
+                    />
+                    <Icon name="logo-facebook" size={36} style={{ color: "#ef7d00" }}
+                        onPress={() => Linking.openURL("https://www.facebook.com/JollyIT")}
+                    />
+                    <Icon name="logo-linkedin" size={36} style={{ color: "#ef7d00" }}
+                        onPress={() => Linking.openURL("https://www.linkedin.com/company/jolly-it-solutions-ltd")}
+                    />
+                    <Icon name="logo-youtube" size={36} style={{ color: "#ef7d00" }}
+                        onPress={() => Linking.openURL("https://www.youtube.com/channel/UCAX1Fq1g7mQv82ZQ0vO-84A")} />
+                </Animated.View>
+                <Animated.View style={{
+                    marginTop: 10,
+                    fontFamily: Fonts.RobotoLight,
+                    borderRadius: 5,
+                    textAlign: "center",
+                    backgroundColor: "#2A2F33",
+                    opacity: showAnim,
+                    width: 320,
+                    padding: 5,
+                    justifyContent: "space-between",
+                    transform: [
+                        {
+                            translateX: showAnim.interpolate({
+                                inputRange: [0, 1],
+                                outputRange: [20, 1]
+                            })
+                        },
+                        {
+                            translateY: showAnim.interpolate({
+                                inputRange: [0, 1],
+                                outputRange: [20, 1]
+                            })
+                        }
+                    ]
+                }}>
+                    {numOfCustomers && <View>
+                        <Text style={styles.LMIText}>
                             <IconFontAwesome name="support" size={25} style={{ color: "#ef7d00" }} />  Currently Helping
-                            <Text style={{ color: "#ef7d00", fontFamily: Fonts.OpenSansConBold }}> {this.state.numOfCustomers} </Text>
+                            <Text style={{ color: "#ef7d00", fontFamily: Fonts.OpenSansConBold }}> {numOfCustomers} </Text>
                             Customers
-                            </Text>}
-                    </Animated.View>
+                            </Text>
+                        <Text style={{ color: "white", fontFamily: Fonts.OpenSansConBold, display: "flex", justifyContent: "flex-end" }}> Last Updated: {lastUpdated} </Text>
+                    </View>}
+                </Animated.View>
 
-                    <Animated.View style={{
-                        justifyContent: "space-between",
-                        marginTop: 10,
-                        fontFamily: Fonts.RobotoLight,
-                        borderRadius: 5,
-                        textAlign: "center",
-                        backgroundColor: "#2A2F33",
-                        opacity: this.state.showAnim,
-                        width: 320,
-                        padding: 5,
-                        opacity: this.state.showAnim, transform: [
-                            {
-                                translateX: this.state.showAnim.interpolate({
-                                    inputRange: [0, 1],
-                                    outputRange: [20, 1]
-                                })
-                            },
-                            {
-                                translateY: this.state.showAnim.interpolate({
-                                    inputRange: [0, 1],
-                                    outputRange: [20, 1]
-                                })
-                            }
-                        ]
-                    }}>
-                        {this.state.numOfCustomers && <Text style={styles.LMIText}>
-                            <IconFontAwesome name="bar-chart" size={25} style={{ color: "#ef7d00" }} /> 4.96 / <Text style={{ fontFamily: Fonts.RobotoBold, color: "#ef7d00" }}>5</Text> Satisfaction Rate
+                <Animated.View style={{
+                    justifyContent: "space-between",
+                    marginTop: 10,
+                    fontFamily: Fonts.RobotoLight,
+                    borderRadius: 5,
+                    textAlign: "center",
+                    backgroundColor: "#2A2F33",
+                    opacity: showAnim,
+                    width: 320,
+                    padding: 5,
+                    opacity: showAnim, transform: [
+                        {
+                            translateX: showAnim.interpolate({
+                                inputRange: [0, 1],
+                                outputRange: [20, 1]
+                            })
+                        },
+                        {
+                            translateY: showAnim.interpolate({
+                                inputRange: [0, 1],
+                                outputRange: [20, 1]
+                            })
+                        }
+                    ]
+                }}>
+                    {numOfCustomers && <Text style={styles.LMIText}>
+                        <IconFontAwesome name="bar-chart" size={25} style={{ color: "#ef7d00" }} /> 4.96 / <Text style={{ fontFamily: Fonts.RobotoBold, color: "#ef7d00" }}>5</Text> Satisfaction Rate
                             </Text>}
-                    </Animated.View>
-                </View>
-            </ImageBackground >
-        );
-    }
-} //<ion-icon name="help-buoy"></ion-icon>
+                </Animated.View>
+            </View>
+        </ImageBackground >
+    );
+}
 
 const styles = StyleSheet.create({
     container: {
@@ -219,10 +238,12 @@ const styles = StyleSheet.create({
     },
     LMIText: {
         color: "white",
-        fontSize: 22,
+        fontSize: Dimensions.get("window").width > 400 ? 52 : 22,
         fontFamily: Fonts.RobotoLight,
         padding: 0,
-        margin: 0
+        margin: 0,
+        justifyContent: "center",
+        display: "flex"
     }
 })
 
